668df53bc7c3e3e7b27cb90b978148b02ab6945c
nemflores <jackfive95@gmail.com>
2019-10-03 15:00:02 -0700

    new

---
+++ Meme/sketch.js
@@ -0,0 +1,23 @@
+/*
+meme version 1
+ 10.3.2019
+ */
+function draw () {
+    createCanvas(640, 360);
+}
+
+
+function draw(){
+    background("blue");
+    fill('black')
+    
+    textSize(100);
+    text("MMP 210",100, 100);
+    
+    textSize(100);
+    fill ("purple");
+    stroke('pink');
+    strokeWeight(10);
+     
+    text("hello world", 100,200);
+}
