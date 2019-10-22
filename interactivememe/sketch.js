af8857d29652b3fbbdf6f3184cced0a422dbd5ed
nemflores <jackfive95@gmail.com>
2019-10-22 13:47:14 -0700

    new

--- interactivememe/sketch.js
+++ interactivememe/sketch.js
@@ -9,6 +9,8 @@ function setup() {
 var img;
 var img2;
 var img3;
+var x= 600, y=500 ;
+var speed = 2;
 
 function preload() {
     img = loadImage("obi-wan.jpg");
@@ -17,6 +19,7 @@ function preload() {
 }
 
 function draw() {
+   
     image(img, 0, 0);
     image(img2, 320, 0);
     image(img3, 690, 0);
@@ -24,20 +27,24 @@ function draw() {
     textSize(20);
     fill('white');
     textStyle(NORMAL);
-    stroke('black');
+    stroke('white');
     strokeWeight(10);
     textFont("Grenze");
-
+    stroke("black");
+     text("When your dad gets your report card",x, 500);
+    fill("black");
+    x +=speed;
+    if (x > width) {
+        x=100;
+    }
     if (mouseIsPressed && mouseX < 320) {
+        fill("white");
         text("Wasup Pop", 111, 50);
     } else if (mouseIsPressed && mouseX < 690) {
-        text("Your report just came in you mind explaining", 320, 50);
+        fill('white');
+        text("Your report card just came in, take a look", 350, 50);
     }
      else if (mouseIsPressed ) {
+         fill("white");
         text("You see what had happened was", 700, 50);
-     }
-
-
-
-   
-}
+     }}
