5f9077d53f5946b640fe338c8fdb6535706ff1c4
nemflores <jackfive95@gmail.com>
2019-12-05 12:56:18 -0800

    new

--- presentation/sketch.js
+++ presentation/sketch.js
@@ -11,11 +11,11 @@ var slideNumber = 0;
 var totalSlides = 3;
 
 var nextBtnX = 700;
-var nextBtnY = 500;
+var nextBtnY = 550;
 var nextBtnSize = 50;
 
-var prevBtnX = 100;
-var prevBtnY = 500;
+var prevBtnX = 50;
+var prevBtnY = 550;
 var prevBtnW = 100;
 var prevBtnH = 50;
 
@@ -30,7 +30,7 @@ function setup() {
 }
 
 function draw() {
-    background(#0000FF);
+    background(0,0,150);
  
     if (slideNumber == 0) {
         // content for slide number 0
@@ -46,23 +46,19 @@ function draw() {
         // content for slide number 1
         
         fill(255);
-        textSize(30);
+        textSize(20);
         textAlign(CENTER, CENTER);
-        text("For my Project will be using", width/2, 100);
-        
-        ellipse(200, 400, 100);
-        rect(200, 375, 200, 50);
-        fill(0);
-        ellipse(200, 400, 80);
+        text("For my Project will be using three potentiometers to Analog Pins 0, 1, and 2 on my Arduino (sketch) and printed their values as ASCII strings separated by commas and capped off with a carriage return and newline (a la println()) to my P5 sketch.", 150, 150, 250, 250);
+       
         
         textSize(20);
-        text("The user hits the FSR to eject the cats from the litter box.", width/2, height - 100);
+        text("You will be able to draw anything using the potentiometers as well as change the colors.", width/2, height - 100);
     
     } else if (slideNumber == 2) {
         
         // theme
         image(Galaxy, 0, 0, width, height);
-        text("Theme", width/2, height/2);
+        text("The Setup", width/2, height/2);
     }
     
     
@@ -76,7 +72,7 @@ function draw() {
         fill(0);
         textSize(20);
         textAlign(CENTER, CENTER);
-        text("Next", nextBtnX, nextBtnY);
+        text("Next", nextBtnX, nextBtnY,);
     }
     
     
